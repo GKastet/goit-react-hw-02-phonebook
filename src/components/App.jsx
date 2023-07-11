9242f4088391e4a5425c5a8604fc98a4eeda4e65
GKastet <127600582+GKastet@users.noreply.github.com>
2023-07-11 13:33:05 +0200

    hw-02-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+//import Form from './Form/Form';
+//import Contacts from './Contacts/Contacts';
+import Container from './Container/Container';
+import { nanoid } from 'nanoid';
+
+const INITIAL_STATE = {
+  contacts: [],
+  filter: '',
 };
+
+export class App extends Component {
+  state = {
+    ...INITIAL_STATE,
+  };
+
+  formAddContact = contactData => {
+    const contact = { id: nanoid(), ...contactData };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  handleOnChangeFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value });    
+  };
+  getFilteredContact =()=>{
+    const {contacts, filter} = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+  }
+  onRemoveContact = contactId => {
+    this.setState({      
+      contacts: this.state.contacts.filter(contact => contact.id !== contactId),
+    });
+  };
+  
+  render() {
+    
+    const filteredContact = this.getFilteredContact();
+    return (
+      <>
+        <Container
+          formAddContact={this.formAddContact}
+          value={this.state.filter}
+          handleOnChangeFilter={this.handleOnChangeFilter}
+          filteredContact={filteredContact}
+          contactsArr = {this.state.contacts}
+          onRemoveContact={this.onRemoveContact}
+        />        
+      </>
+    );
+  }
+}
